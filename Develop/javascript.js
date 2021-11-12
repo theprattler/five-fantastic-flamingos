d14feeb1cc16c614aff6c7f68740b8264130af3d
The Prattler <jmichaelsmith@juno.com>
2021-11-12 12:05:29 -0500

    task texts saved to local storage

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -87,3 +87,34 @@ if (17 < currentHour) {
     $("#17").removeClass("past future");
 };
 
+// save button is clicked
+$(".saveBtn").click(function() {
+    var task9 = $("#task9").val();
+    var task10 = $("#task10").val();
+    var task11 = $("#task11").val();
+    var task12 = $("#task12").val();
+    var task13 = $("#task13").val();
+    var task14 = $("#task14").val();
+    var task15 = $("#task15").val();
+    var task16 = $("#task16").val();
+    var task17 = $("#task17").val();
+
+    var tasks = {
+        9: task9,
+        10: task10,
+        11: task11,
+        12: task12,
+        13: task13,
+        14: task14,
+        15: task15,
+        16: task16,
+        17: task17
+    };
+    localStorage.setItem("work-day-scheduler", JSON.stringify(tasks));
+
+    //saveTask();
+});
+
+//var saveTask = function() {
+    
+//};
