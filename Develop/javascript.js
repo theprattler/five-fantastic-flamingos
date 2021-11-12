a9eb983588c3384ae8dc5e2a3ace9a8669f9dc93
The Prattler <jmichaelsmith@juno.com>
2021-11-12 14:14:58 -0500

    retrieve tasks from local storage

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -99,22 +99,41 @@ $(".saveBtn").click(function() {
     var task16 = $("#task16").val();
     var task17 = $("#task17").val();
 
+    // array for tasks
     var tasks = {
-        9: task9,
-        10: task10,
-        11: task11,
-        12: task12,
-        13: task13,
-        14: task14,
-        15: task15,
-        16: task16,
-        17: task17
+        hour9: task9,
+        hour10: task10,
+        hour11: task11,
+        hour12: task12,
+        hour13: task13,
+        hour14: task14,
+        hour15: task15,
+        hour16: task16,
+        hour17: task17
     };
+    // store data in local storage
     localStorage.setItem("work-day-scheduler", JSON.stringify(tasks));
-
-    //saveTask();
 });
 
-//var saveTask = function() {
-    
-//};
+// retrieve tasks from local storage
+var savedTasks = localStorage.getItem("work-day-scheduler");
+    var task9 = JSON.parse(savedTasks);
+    document.getElementById("task9").innerHTML = task9.hour9;
+    var task10 = JSON.parse(savedTasks);
+    document.getElementById("task10").innerHTML = task10.hour10;
+    var task11 = JSON.parse(savedTasks);
+    document.getElementById("task11").innerHTML = task11.hour11;
+    var task12 = JSON.parse(savedTasks);
+    document.getElementById("task12").innerHTML = task12.hour12;
+    var task13 = JSON.parse(savedTasks);
+    document.getElementById("task13").innerHTML = task13.hour13;
+    var task14 = JSON.parse(savedTasks);
+    document.getElementById("task14").innerHTML = task14.hour14;
+    var task15 = JSON.parse(savedTasks);
+    document.getElementById("task15").innerHTML = task15.hour15;
+    var task16 = JSON.parse(savedTasks);
+    document.getElementById("task16").innerHTML = task16.hour16;
+    var task17 = JSON.parse(savedTasks);
+    document.getElementById("task17").innerHTML = task17.hour17;
+    var task18 = JSON.parse(savedTasks);
+    document.getElementById("task18").innerHTML = task18.hour18;
